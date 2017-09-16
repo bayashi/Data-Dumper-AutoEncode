07561c6899a957ae59c491be8f35c6cbe1188b3e
bayashi <bayashi@cpan.org>
2017-09-16 14:57:23 +0900

    bump version

--- lib/Data/Dumper/AutoEncode.pm
+++ lib/Data/Dumper/AutoEncode.pm
@@ -9,7 +9,7 @@ use Data::Dumper; # Dumper
 use parent qw/Exporter/;
 our @EXPORT = qw/eDumper Dumper/;
 
-our $VERSION = '0.2';
+our $VERSION = '0.3';
 
 our $ENCODING = '';
 our $CHECK_ALREADY_ENCODED = 0;
